628a93698eb9e4bd5016f68884fa48f58571bd4f
sedrickel <sedricke15@gmail.com>
2022-11-17 17:35:41 +0800

    try to change

--- Rafflesia_SDM.R
+++ Rafflesia_SDM.R
@@ -27,6 +27,7 @@ getDoParWorkers()
 
 setwd("D:/PROJECTS/Rafflesia_SDM")
 
+
 ### TRUE SKILL STATISTIC TEST FUNCTION #####
 #to be able to run this script you need to have told the Maxent model to produce background predictions. If you are running MaxEnt in R this means putting the argument (after "args") "writebackgroundpredictions=true" as true not false. 
 
