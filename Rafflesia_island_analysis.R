fb78600595dd32fc780d782edb4174f8d923c0d7
sedrickel <sedricke15@gmail.com>
2023-01-12 17:12:00 +0800

    added native range data extraction code

--- Rafflesia_island_analysis.R
+++ Rafflesia_island_analysis.R
@@ -228,3 +228,139 @@ change.summ[3,7] <- change.summ[2,7]/(change.summ[1,7] + change.summ[2,7])*100
 change.summ
 
 write.csv(change.summ, file= paste0(sp.name,"_pixel_changes.csv"), row.names = F)
+
+
+# Extract data for boxplots #
+# this assumes that envi data has been read in from main Rafflesia_SDM codes
+# uses A1 - masked data only #
+
+#speciosa
+spe <- as.data.frame(rspe.cur.a1, na.rm = T, xy=T)
+colnames(spe) <- c("lon", "lat", "bin")
+spe <- filter(spe, bin==1)
+spe <- cbind(spe$lon, spe$lat)
+
+spe.df <- raster::extract(envdata, spe, method = "simple", na.rm = T)
+spe.df <- as.data.frame(spe.df)
+spe.df$Scenario <- rep("Current", dim(spe.df)[1])
+
+#RCP4.5
+spe45 <- as.data.frame(rspe.r45.a1, na.rm = T, xy=T)
+colnames(spe45) <- c("lon", "lat", "bin")
+spe45 <- filter(spe45, bin==1)
+spe45 <- cbind(spe45$lon, spe45$lat)
+
+spe45.df <- raster::extract(envdata, spe45, method = "simple", na.rm = T)
+spe45.df <- as.data.frame(spe45.df)
+spe45.df$Scenario <- rep("RCP4.5", dim(spe45.df)[1])
+
+
+#RCP 8.5
+spe85 <- as.data.frame(rspe.r85.a1, na.rm = T, xy=T)
+colnames(spe85) <- c("lon", "lat", "bin")
+spe85 <- filter(spe85, bin==1)
+spe85 <- cbind(spe85$lon, spe85$lat)
+
+spe85.df <- raster::extract(envdata, spe85, method = "simple", na.rm = T)
+spe85.df <- as.data.frame(spe85.df)
+spe85.df$Scenario <- rep("RCP8.5", dim(spe85.df)[1])
+
+
+#combine all data from one species
+
+allspe <- rbind(spe.df, spe45.df, spe85.df)
+allspe$Species <- rep("Rspe", dim(allspe)[1]) #change for each species
+
+summary(allspe)
+
+## R. lagascae
+#current
+lag <- as.data.frame(rlag.cur.a1, na.rm = T, xy=T)
+colnames(lag) <- c("lon", "lat", "bin")
+lag <- filter(lag, bin==1)
+lag <- cbind(lag$lon, lag$lat)
+
+lag.df <- raster::extract(envdata, lag, method = "simple", na.rm = T)
+lag.df <- as.data.frame(lag.df)
+lag.df$Scenario <- rep("Current", dim(lag.df)[1])
+
+#RCP4.5
+lag45 <- as.data.frame(rlag.r45.a1, na.rm = T, xy=T)
+colnames(lag45) <- c("lon", "lat", "bin")
+lag45 <- filter(lag45, bin==1)
+lag45 <- cbind(lag45$lon, lag45$lat)
+
+lag45.df <- raster::extract(envdata, lag45, method = "simple", na.rm = T)
+lag45.df <- as.data.frame(lag45.df)
+lag45.df$Scenario <- rep("RCP4.5", dim(lag45.df)[1])
+
+
+#RCP 8.5
+lag85 <- as.data.frame(rlag.r85.a1, na.rm = T, xy=T)
+colnames(lag85) <- c("lon", "lat", "bin")
+lag85 <- filter(lag85, bin==1)
+lag85 <- cbind(lag85$lon, lag85$lat)
+
+lag85.df <- raster::extract(envdata, lag85, method = "simple", na.rm = T)
+lag85.df <- as.data.frame(lag85.df)
+lag85.df$Scenario <- rep("RCP8.5", dim(lag85.df)[1])
+
+
+#combine all data from one species
+
+alllag <- rbind(lag.df, lag45.df, lag85.df)
+alllag$Species <- rep("Rlag", dim(alllag)[1]) #change for each species
+summary(alllag)
+
+## R. lobata
+#current
+lob <- as.data.frame(rlob.cur.a1, na.rm = T, xy=T)
+colnames(lob) <- c("lon", "lat", "bin")
+lob <- filter(lob, bin==1)
+lob <- cbind(lob$lon, lob$lat)
+
+lob.df <- raster::extract(envdata, lob, method = "simple", na.rm = T)
+lob.df <- as.data.frame(lob.df)
+lob.df$Scenario <- rep("Current", dim(lob.df)[1])
+
+#RCP4.5
+lob45 <- as.data.frame(rlob.r45.a1, na.rm = T, xy=T)
+colnames(lob45) <- c("lon", "lat", "bin")
+lob45 <- filter(lob45, bin==1)
+lob45 <- cbind(lob45$lon, lob45$lat)
+
+lob45.df <- raster::extract(envdata, lob45, method = "simple", na.rm = T)
+lob45.df <- as.data.frame(lob45.df)
+lob45.df$Scenario <- rep("RCP4.5", dim(lob45.df)[1])
+
+
+#RCP 8.5
+lob85 <- as.data.frame(rlob.r85.a1, na.rm = T, xy=T)
+colnames(lob85) <- c("lon", "lat", "bin")
+lob85 <- filter(lob85, bin==1)
+lob85 <- cbind(lob85$lon, lob85$lat)
+
+lob85.df <- raster::extract(envdata, lob85, method = "simple", na.rm = T)
+lob85.df <- as.data.frame(lob85.df)
+lob85.df$Scenario <- rep("RCP8.5", dim(lob85.df)[1])
+
+#combine all data from one species
+
+alllob <- rbind(lob.df, lob45.df, lob85.df)
+alllob$Species <- rep("Rlob", dim(alllob)[1]) #change for each species
+
+summary(alllob)
+
+#remove NAs
+allspe <- na.omit(allspe)
+alllag <- na.omit(alllag)
+alllob <- na.omit(alllob)
+
+#combine all species df
+all.raf <- rbind(allspe, alllag, alllob)
+all.raf$Species <- as.factor(all.raf$Species)
+all.raf$Scenario <- as.factor(all.raf$Scenario)
+summary(all.raf)
+
+write.csv(all.raf, file = "All_Rafflesia_spp_a1_suitable_masked_native_ranges_envdata.csv", row.names = F)
+
